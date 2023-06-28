cdd6d509845765ae52e7626d88d358d3a0abcc4e
Ondrej Mottl <ondrej.mottl@gmail.com>
2023-06-28 16:38:08 +0200

    render: add option to inlcude button to download Rmd file

--- render.R
+++ render.R
@@ -19,7 +19,8 @@ library(here)
 # helper function o render fie in both formats
 render_md_and_html <- function(
     file_name,
-    file_name_html = file_name) {
+    file_name_html = file_name,
+    code_download = FALSE) {
   quarto::quarto_render(
     input = here::here(
       paste0(file_name, ".qmd")
@@ -32,7 +33,9 @@ render_md_and_html <- function(
     input = here::here(
       paste0(file_name, ".qmd")
     ),
-    output_format = rmarkdown::html_document(),
+    output_format = rmarkdown::html_document(
+      code_download = code_download
+    ),
     output_file = here::here(
       paste0("docs/", file_name_html, ".html")
     )
@@ -46,7 +49,7 @@ render_md_and_html(file_name = "README", file_name_html = "index")
 render_md_and_html(file_name = "workshop_info")
 
 # pre_workshop -----
-render_md_and_html(file_name = "pre_workshop")
+render_md_and_html(file_name = "pre_workshop", code_download = TRUE)
 
 # step_by_step_guide -----
-render_md_and_html(file_name = "step_by_step_guide")
+render_md_and_html(file_name = "step_by_step_guide", code_download = TRUE)
